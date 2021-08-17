f11892af276f123882f8f36cf084260ba7305b8e
patsch9 <47031716+patsch9@users.noreply.github.com>
2021-08-17 12:18:18 +0200

    rtsp edition

---
+++ ring_hassio/main.mjs
@@ -0,0 +1,38 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { readFile, writeFile } from 'fs/promises';
+import ringClientApi from 'ring-client-api';
+
+const CONFIG_PATH = process.env.CONFIG_PATH || '/etc/ring-ffmpeg.json';
+
+async function updateSavedToken({ newRefreshToken, oldRefreshToken }) {
+	console.log('Refresh Token Updated: ', newRefreshToken);
+	if (!oldRefreshToken) return;
+	const currentConfig = await readFile(CONFIG_PATH, 'utf8');
+	const updatedConfig = currentConfig.replace(oldRefreshToken, newRefreshToken);
+	await writeFile(CONFIG_PATH, updatedConfig);
+}
+
+const ffmpegArgs = { 
+	audio: [], video: [],
+	output: process.argv.slice(2),
+};
+
+async function main() {
+	const { refreshToken, ffmpegPath } = JSON.parse(await readFile(CONFIG_PATH, 'utf8'));
+	const ringApi = new ringClientApi.RingApi({ ffmpegPath, refreshToken });
+	ringApi.onRefreshTokenUpdated.subscribe(updateSavedToken);
+	const [camera] = await ringApi.getCameras();
+	const session = await camera.createSipSession({ skipFfmpegCheck: true });
+	await session.start(ffmpegArgs);
+}
+
+// handle exit signals
+for (const signal of ['SIGINT','SIGTERM','SIGQUIT']) {
+	process.on(signal, () => {
+		console.log(`Received ${signal}, exiting...`);
+		process.exit();
+	});
+}
+
+main();
